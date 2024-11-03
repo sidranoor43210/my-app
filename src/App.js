1b9e411183deee2a99a53762b25572caea75df95
Sidra Noor <sidranoor43210@gmail.com>
2024-11-03 17:07:47 +0500

    react 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='style'>
+      <Welcome name="Sara" />      <Welcome name="Payal" />      <Welcome name="Aditi" />  
+      <button> Click Here! </button>
+      
+      
+        </div>
   );
 }
 
